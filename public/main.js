e5c1df6ab880a9258360afda9648e7da0da4d6e1
Justin <fallingreign@gmail.com>
2020-08-24 09:27:05 +1000

    widget button

--- public/main.js
+++ public/main.js
@@ -2,12 +2,31 @@ miro.onReady(() => {
     miro.initialize({
       extensionPoints: {
         bottomBar: {
-          title: 'Extra notes',
-          svgIcon: '<path fill="currentColor" d="M12 2.617L2.617 12 12 21.383 21.383 12 12 2.617zm.707-2.121l10.797 10.797a1 1 0 0 1 0 1.414L12.707 23.504a1 1 0 0 1-1.414 0L.496 12.707a1 1 0 0 1 0-1.414L11.293.496a1 1 0 0 1 1.414 0z"></path>',
+          title: 'Estimates',
+          svgIcon: Config.icon,
           onClick: function () {
             miro.board.ui.openLeftSidebar('public/sidebar.html')
           }
-        }
+        },
+        getWidgetMenuItems: (widgets) => {
+
+          const supportedWidgetsInSelection = widgets
+              .filter((widget) => Config.supported_widgets[widget.type.toLowerCase()] !== undefined);
+
+          // All selected widgets have to be supported in order to show the menu
+          if (supportedWidgetsInSelection.length == widgets.length) {
+              return Promise.resolve([{
+                  tooltip: 'Estimate',
+                  svgIcon: Config.icon,
+                  onClick: async (widgets) => {
+                    await miro.board.ui.openModal('modal.html', Config.modal_options)
+                  }
+              }])
+          }
+
+          // Not all selected widgets are supported, we won't show the menu
+          return Promise.resolve([{}]);
+      }
       }
     })
   })
