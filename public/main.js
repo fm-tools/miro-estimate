b0fd9d694618079517f804e81319e770f67305dc
Justin <fallingreign@gmail.com>
2020-10-09 12:54:02 +1100

    test selection listener

--- public/main.js
+++ public/main.js
@@ -1,33 +1,41 @@
 miro.onReady(() => {
-    miro.initialize({
-      extensionPoints: {
-        bottomBar: {
-          title: 'Estimates',
-          svgIcon: Config.icon,
-          onClick: function () {
-            miro.board.ui.openLeftSidebar('public/sidebar.html')
+  miro.initialize({
+    extensionPoints: {
+      bottomBar: {
+        title: 'Estimates',
+        svgIcon: Config.icon,
+        onClick: function () {
+          miro.board.ui.openLeftSidebar('public/sidebar.html')
+        }
+      },
+      getWidgetMenuItems: (widgets) => {
+
+        const supportedWidgetsInSelection = widgets
+          .filter((widget) => Config.supported_widgets[widget.type.toLowerCase()] !== undefined);
+
+        // All selected widgets have to be supported in order to show the menu
+        if (supportedWidgetsInSelection.length == widgets.length) {
+          return {
+            tooltip: 'Estimate',
+            svgIcon: Config.icon,
+            onClick: (widgets) => {
+              miro.board.ui.openBottomPanel('public/bottompanel.html', Config.bottompanel_options)
+            }
           }
-        },
-        getWidgetMenuItems: (widgets) => {
+        }
 
-          const supportedWidgetsInSelection = widgets
-              .filter((widget) => Config.supported_widgets[widget.type.toLowerCase()] !== undefined);
-
-          // All selected widgets have to be supported in order to show the menu
-          if (supportedWidgetsInSelection.length == widgets.length) {
-              return {
-                  tooltip: 'Estimate',
-                  svgIcon: Config.icon,
-                  onClick: (widgets) => {
-                    miro.board.ui.openBottomPanel('public/bottompanel.html', Config.bottompanel_options)
-                  }
-              }
-          }
-
-          // Not all selected widgets are supported, we won't show the menu
-          return {};
+        // Not all selected widgets are supported, we won't show the menu
+        return {};
       }
-      }
-    })
+    }
   })
-  
+
+  miro.addListener('SELECTION_UPDATED', async function (event) {
+    let widgets = await miro.board.selection.get();
+    if (widgets.length > 0) {
+        isPanelOpen = miro.board.ui.openBottomPanel('public/bottompanel.html', Config.bottompanel_options);
+    } else {
+        await miro.board.ui.closeBottomPanel();
+    }
+});
+})
