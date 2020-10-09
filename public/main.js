138f65355dd4a1f1179c614df23245d6bd118ea5
Justin <fallingreign@gmail.com>
2020-10-09 13:01:35 +1100

    remove widget bar icon

--- public/main.js
+++ public/main.js
@@ -7,33 +7,22 @@ miro.onReady(() => {
         onClick: function () {
           miro.board.ui.openLeftSidebar('public/sidebar.html')
         }
-      },
-      getWidgetMenuItems: (widgets) => {
-
-        const supportedWidgetsInSelection = widgets
-          .filter((widget) => Config.supported_widgets[widget.type.toLowerCase()] !== undefined);
-
-        // All selected widgets have to be supported in order to show the menu
-        if (supportedWidgetsInSelection.length == widgets.length) {
-          return {
-            tooltip: 'Estimate',
-            svgIcon: Config.icon,
-            onClick: (widgets) => {
-              miro.board.ui.openBottomPanel('public/bottompanel.html', Config.bottompanel_options)
-            }
-          }
-        }
-
-        // Not all selected widgets are supported, we won't show the menu
-        return {};
       }
     }
   })
 
   miro.addListener('SELECTION_UPDATED', async function (event) {
     let widgets = await miro.board.selection.get();
+
     if (widgets.length > 0) {
-        isPanelOpen = miro.board.ui.openBottomPanel('public/bottompanel.html', Config.bottompanel_options);
+      const supportedWidgetsInSelection = widgets
+      .filter((widget) => Config.supported_widgets[widget.type.toLowerCase()] !== undefined);
+
+      // All selected widgets have to be supported in order to show the menu
+      if (supportedWidgetsInSelection.length == widgets.length) {
+        miro.board.ui.openBottomPanel('public/bottompanel.html', Config.bottompanel_options);
+      }
+
     } else {
         await miro.board.ui.closeBottomPanel();
     }
