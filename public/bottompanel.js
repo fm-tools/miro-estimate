b3b331962be56c5994aec640e6bf7db6085259de
Justin <fallingreign@gmail.com>
2020-10-09 12:14:33 +1100

    isNumeric check

--- public/bottompanel.js
+++ public/bottompanel.js
@@ -7,3 +7,11 @@ function changeInputValue(element, delta) {
 
     element.value = (parseFloat(estimate) + parseInt(delta)).toString();
 }
+
+function isNumeric(value) {
+    if (isNaN(parseFloat(value)) || ! isFinite(value)) {
+        return false;
+    }
+
+    return true;
+}
